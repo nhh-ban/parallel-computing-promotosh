48e1482079247ef5ea7b775e2b8aab061a1bb63a
BARUA <72656168+promotosh@users.noreply.github.com>
2023-11-02 01:19:04 +0100

    Update timer.r

--- scripts/timer.r
+++ scripts/timer.r
@@ -1,1 +1,17 @@
+library(tictoc)
+
+# Time the original solution
+tic("Original Solution")
+source("scripts/solution_original.R")
+toc()
+
+# Time the parallel loop solution
+tic("Parallel Loop Solution")
+source("scripts/solution_parallel_loop.R")
+toc()
+
+# Time the parallel function solution
+tic("Parallel Function Solution")
+source("scripts/solution_parallel_function.R")
+toc()
 
