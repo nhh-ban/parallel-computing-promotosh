c63e7c052ea09df92b85bbaa7a0a6381046a2793
BARUA <72656168+promotosh@users.noreply.github.com>
2023-11-02 01:31:00 +0100

    Update parallel_function.R

--- scripts/parallel_function.R
+++ scripts/parallel_function.R
@@ -1,8 +1,28 @@
-MTweedieTests <- function(...) {
-    # ... existing function code ...
-    results <- foreach(i=1:M, .combine=rbind) %dopar% {
-        # simulation code
-    }
-    return(results)
+library(tweedie) 
+library(ggplot2)
+library(foreach)
+library(doParallel)
+
+simTweedieTest <-  
+  function(N){ 
+    t.test( 
+      rtweedie(N, mu=10000, phi=100, power=1.9), 
+      mu=10000 
+    )$p.value 
+  } 
+
+MTweedieTests_parallel <- function(N, M, sig) {
+  cl <- makeCluster(detectCores() - 1)  # create a cluster
+  registerDoParallel(cl)
+  
+  results <- foreach(m=1:M, .combine=c, .packages='tweedie') %dopar% {
+    simTweedieTest(N)
+  }
+  
+  stopCluster(cl)
+  sum(results < sig) / M
 }
 
+df <-  
+  expand.grid( 
+    N
