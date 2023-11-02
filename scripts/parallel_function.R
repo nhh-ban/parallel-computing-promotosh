1032e361b1f83ba78e41d30fa7e6f2d089ad2438
BARUA <72656168+promotosh@users.noreply.github.com>
2023-11-02 01:17:30 +0100

    Update parallel_function.R

--- scripts/parallel_function.R
+++ scripts/parallel_function.R
@@ -1,1 +1,8 @@
+MTweedieTests <- function(...) {
+    # ... existing function code ...
+    results <- foreach(i=1:M, .combine=rbind) %dopar% {
+        # simulation code
+    }
+    return(results)
+}
 
